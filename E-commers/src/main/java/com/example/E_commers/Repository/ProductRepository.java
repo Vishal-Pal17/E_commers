864a1eefd9941b29704f5a2800d87374ae871463
Vishal <vpal32460@gmail.com>
2025-11-27 16:14:29 +0530

    "Initial commit"

--- E-commers/src/main/java/com/example/E_commers/Repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.E_commers.Repository;
-
-import com.example.E_commers.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product , Long> {
-}
